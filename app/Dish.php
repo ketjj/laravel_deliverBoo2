e40f2a3c738f8319fc2f18fdf17dad3e1ed41cd5
ketjj <jorjolianiket@gmail.com>
2022-09-04 19:59:40 +0200

    create pivot dish_order(FK-s) + Model(functions)

--- app/Dish.php
+++ app/Dish.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dish extends Model
 {
-    //
+    //RELATIONS------with Order(n:n)
+
+    public function orders(){
+        return $this->belongsToMany('App\Order');
+    }
 }
--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    //RELATIONS------with Dish (n:n)
+    public function dishes(){
+        return $this->belongsToMany('App\Dish');
+    }
 }
