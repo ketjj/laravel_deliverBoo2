6d1c3cbb937eebb04b6261f53eba23d90c2fe0c9
ketjj <jorjolianiket@gmail.com>
2022-09-04 18:39:16 +0200

    intall ui & vue --auth + .env(db)

---
+++ resources/indications.blade.php
@@ -0,0 +1,11 @@
+1. install:
+
+  composer require laravel/ui:^2.4
+  php artisan ui vue --auth
+
+2. Insert DB file-name into .env file:
+
+  DB_DATABASE=db_name
+  DB_USERNAME=root
+  DB_PASSWORD=root   
+   
